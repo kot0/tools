e5c99bd73c9287d7e46cd4ba87fe5b8859736bb4
kot0 <47835003+kot0@users.noreply.github.com>
2019-05-17 17:48:32 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import "github.com/tidwall/gjson"
 import "compress/zlib"
 import "bytes"
 import "io/ioutil"
+import "fmt"
 
 const UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36"
 
